6c2e2db43506e607835eb7640f34fd90eeaec305
Peter Paker <peterpaker611@gmail.com>
2023-05-09 22:27:28 -0700

    'backend/com'

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -25,8 +25,6 @@ const HARDHAT_NETWORK_ID = '31337';
 // This is an error code that indicates that the user canceled a transaction
 const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
 
-// This section is contained both integration.
-
 // This component is in charge of doing these things:
 //   1. It connects to the user's wallet
 //   2. Initializes ethers and the Token contract
