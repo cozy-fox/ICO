593cf62fa838a345e0b132766c58c12120ea8c16
Oliver Cawson <cawsonoliver33@gmail.com>
2023-05-09 17:52:34 -0700

    update/frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Dapp } from "./components/Dapp";
 
 // We import bootstrap here, but you can remove if you want
 import "bootstrap/dist/css/bootstrap.css";
+import "./global.scss";
 
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
